6cd5d8803008ebff567bcdea42cc8170f2917c1f
Ayn <2312709@dlu.edu.vn>
2026-09-25 18:22:49 +0700

    Zombie Attack & Damage Animations

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -1,65 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
-    public Transform player;
-
-    public float DetectionRange = 10f;
-    public float moveSpeed = 2f;
-    public float stopDistance = 2f;
-    public float rotSpeed = 10f;
-
+    public Transform target;
+    public bool Runner = false;
 
     [Header("Health")]
     public int currentHealth;
     public int maxHealth = 100;
 
+    private NavMeshAgent agent;
+    Animator anim;
+
     bool dead;
     private void Start()
     {
         currentHealth = maxHealth;
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+
     }
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(transform.position, player.position);
-
-        if (distance <= DetectionRange && !dead)
+        if (dead)return;
+        anim.SetFloat("Speed",agent.velocity.magnitude);
+        anim.SetBool("Run",Runner);
+        if(currentHealth <= 0)
         {
-            FollowPlayer(distance);
-        }
+            dead = true;
+            currentHealth = 0;
+            agent.ResetPath();
+            agent.speed = 0;
 
-        if (dead)
-        {
-            GetComponent<MeshRenderer>().material.color = Color.red;
-        }
-    }
-    void FollowPlayer(float distance)
-    {
-        Vector3 direction = (player.position - transform.position).normalized;
-        direction.y = 0f;
-
-        Quaternion targetRot = Quaternion.LookRotation(direction);
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotSpeed * Time.deltaTime);
-
-        if (distance > stopDistance)
-        {
-            transform.position += direction * moveSpeed * Time.deltaTime;
+            //GetComponent<MeshRenderer>().material.color = Color.red;
+            anim.SetBool("Death", true);
+            Destroy(gameObject, 5f);
         }
     }
-
+    
     public void TakeDamage(int Damage)
     {
         currentHealth -= Damage;
 
-        if (currentHealth < 0)
-        {
-            dead = true;
-            currentHealth = 0;
-            GetComponent<MeshRenderer>().material.color = Color.red;
-            Destroy(gameObject, 5f);
-        }
     }
 }
