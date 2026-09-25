bdd432fe475e6463247119b7d8efe410dcd36951
Ayn <2312709@dlu.edu.vn>
2026-09-25 19:22:49 +0700

    Advanced Zombie Models & Navigation

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -12,6 +12,13 @@ public class ZombieAI : MonoBehaviour
     public int currentHealth;
     public int maxHealth = 100;
 
+    [Header("Combat")]
+    public float attackRange = 2f;
+    public float attackCooldown = 1f;
+    public float hitDuration = 0.4f;
+
+    float attackTime = 0f;
+
     private NavMeshAgent agent;
     Animator anim;
 
@@ -28,24 +35,62 @@ public class ZombieAI : MonoBehaviour
     void Update()
     {
         if (dead)return;
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance > attackRange)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+            agent.isStopped = true;
+            TryAttack();
+            
+        }
+        
         anim.SetFloat("Speed",agent.velocity.magnitude);
         anim.SetBool("Run",Runner);
-        if(currentHealth <= 0)
+        if(currentHealth <= 0 && !dead)
         {
-            dead = true;
-            currentHealth = 0;
-            agent.ResetPath();
-            agent.speed = 0;
-
-            //GetComponent<MeshRenderer>().material.color = Color.red;
-            anim.SetBool("Death", true);
-            Destroy(gameObject, 5f);
+            Die();
         }
+    }
+
+    void Die()
+    {
+        dead = true;
+        currentHealth = 0;
+        agent.ResetPath();
+        agent.speed = 0;
+        //GetComponent<MeshRenderer>().material.color = Color.red;
+        anim.SetBool("Death", true);
+        Destroy(gameObject, 5f);
     }
     
     public void TakeDamage(int Damage)
     {
         currentHealth -= Damage;
+        anim.SetTrigger("hit");
+        StartCoroutine(HitDuration());
 
+    }
+    void TryAttack()
+    {
+        if (Time.time >= attackTime)
+        {
+            anim.SetTrigger("Attack");
+            attackTime = Time.time + attackCooldown;
+        }
+    }
+    public void Damage()
+    {
+        Debug.Log("Damaged");
+    }
+    IEnumerator HitDuration()
+    {
+        agent.isStopped = true;
+        yield return new WaitForSeconds(hitDuration);
+        agent.isStopped = false;
     }
 }
